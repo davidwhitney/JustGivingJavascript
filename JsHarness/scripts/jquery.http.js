e221af680b4f13de87c85ade3e01aa78ebc3589f
DalSoft <d.jones@dalsoft.co.uk>
2011-09-24 18:02:54 +0100

    dj

--- JsHarness/scripts/jquery.http.js
+++ JsHarness/scripts/jquery.http.js
@@ -1,21 +1,28 @@
-﻿(function($) {$.extend({
-            
+﻿(function ($) {
+    $.extend({
+
         http: {
-                get: function() {
 
-                    return "GET";
-                },
-                put: function() {
+            get: function (url, callback) {
+                var settings = {
+                    url: url,
+                    dataType: 'jsonp',
+                    success: function (data) { callback(data); }
+                };
+                $.ajax(settings);
+            
+            },
+            put: function () {
 
-                    return "PUT";
-                },
-                head: function() {
-                    return "HEAD";
-                },
-                post: function() {
-                    return "POST";
-                }
+                return "PUT";
+            },
+            head: function () {
+                return "HEAD";
+            },
+            post: function () {
+                return "POST";
             }
-        });
-    
+        }
+    });
+
 })(jQuery);
