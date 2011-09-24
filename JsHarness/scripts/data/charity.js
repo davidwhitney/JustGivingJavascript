4f925a4601e2c3c2c94791c842b361bff5ed59c6
David Whitney <david@davidwhitney.co.uk>
2011-09-24 21:18:36 +0100

    stubs

--- JsHarness/scripts/data/charity.js
+++ JsHarness/scripts/data/charity.js
@@ -6,6 +6,10 @@
             retrieve: function (charityId, overrideCallback) {
                 var location = jg.utils.buildApiLocation("charity/" + charityId);
                 jg.http.get({ url: location, overrideCallback: overrideCallback });
+            },
+            retrieveEvents: function (charityId, overrideCallback) {
+                var location = jg.utils.buildApiLocation("charity/" + charityId + "/events");
+                jg.http.get({ url: location, overrideCallback: overrideCallback });
             }
         }
     });
---
+++ JsHarness/scripts/data/donation.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+
+    $.extend($.data, {
+
+        donation: {
+            retrieve: function (donationId, overrideCallback) {
+                var location = jg.utils.buildApiLocation("donation/" + donationId);
+                jg.http.get({ url: location, overrideCallback: overrideCallback });
+            }
+        }
+    });
+
+} (jQuery));
