a86416c99b31e198e3df29753e9f0e059825bec6
Sam Salisbury <samsalisbury@gmail.com>
2011-09-24 20:52:57 +0100

    Tidying up core.js

--- JsHarness/scripts/core.js
+++ JsHarness/scripts/core.js
@@ -1,30 +1,27 @@
 ﻿var jg = jQuery.noConflict();
 
 (function ($) {
-    $.extend(true, {
+
+    $.extend({
 
         init: function (supplied) {
-            jg.settings = function() {
-                return $.extend({ apiKey: '', rootDomain: 'https://api.justgiving.com', apiVersion: '1' }, supplied);
-            };
+            jg.settings = $.extend({
+                apiKey: '',
+                rootDomain: 'https://api.justgiving.com',
+                apiVersion: '1'
+             }, supplied);
         },
         
-        utils: function () {
-            return new utils();
+        utils: {
+            buildApiLocation: function (locationFormat) {
+                var format = jg.settings.rootDomain + "/{apiKey}/v{apiVersion}/" + locationFormat;
+                format = format.replace("{apiKey}", jg.settings().apiKey);
+                format = format.replace("{apiVersion}", jg.settings().apiVersion);
+                alert(format);
+                return format;
+            }
         }
-});
+
+    });
     
-    
-function utils() {
-
-    this.buildApiLocation = function (locationFormat) {
-
-        var format = jg.settings().rootDomain + "/{apiKey}/v{apiVersion}/" + locationFormat;
-        format = format.replace("{apiKey}", jg.settings().apiKey);
-        format = format.replace("{apiVersion}", jg.settings().apiVersion);
-        alert(format);
-        return format;
-    };
-}
-
-})(jQuery);
+}(jQuery));
