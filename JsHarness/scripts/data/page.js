f30a67540d8c959fdf5cbfb265e2e2e6ec365da3
David Whitney <david@davidwhitney.co.uk>
2011-09-24 18:19:17 +0100

    change sig.

--- JsHarness/scripts/data/page.js
+++ JsHarness/scripts/data/page.js
@@ -11,7 +11,7 @@
 
         this.retrieve = function (pageShortName, overrideCallback) {
             var location = jg.utils().buildApiLocation("fundraising/" + pageShortName);
-            var responseEnvelope = jg.http.get(location, overrideCallback);
+            var responseEnvelope = jg.http.get({url: location, overrideCallback: overrideCallback});
             return responseEnvelope;
         };
     }
