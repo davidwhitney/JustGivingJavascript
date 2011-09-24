0619b37a293f2369440a2dfc0d0f19686aec2cc2
DalSoft <d.jones@dalsoft.co.uk>
2011-09-24 15:15:08 +0100

    Added jquery.http.js

---
+++ JsHarness/scripts/jquery/plugins/jquery.http.js
@@ -0,0 +1,24 @@
+﻿var jg = jQuery.noConflict();
+
+(function($) {
+    $.extend({
+            http: {
+                get: function() {
+
+                    return "GET";
+                },
+                put: function() {
+
+                    return "PUT";
+                },
+                head: function() {
+                    return "HEAD";
+                },
+                post: function() {
+                    return "POST";
+                }
+            }
+        });
+})(jQuery);
+
+
