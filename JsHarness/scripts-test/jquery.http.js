fb72ed6dd7a2559b20cd0181767fc8f87961cc5e
DalSoft <d.jones@dalsoft.co.uk>
2011-09-25 00:20:53 +0100

    dj

--- JsHarness/scripts-test/jquery.http.js
+++ JsHarness/scripts-test/jquery.http.js
@@ -5,9 +5,19 @@
 
     test("ajaxSetup dataType should equal dataType jsonp", function () {
         equal($.ajaxSetup().dataType, 'jsonX');
-    });   
-    
-    
+    });
+
+    asyncTest("Given a vaild Api resource when ", function () {
+        jg.http.get({
+            url: "https://api.justgiving.com/ea902521/v1/fundraising/pages/justgiving",
+            overrideCallback: function (data) {
+                equal(data.pageId, '123');
+                start();
+            }
+        });
+
+    });
+   
     asyncTest("Api result should be return when given a vaild Api resource", function() {
         jg.http.get({
                 url: "https://api.justgiving.com/ea902521/v1/fundraising/pages/justgiving",
