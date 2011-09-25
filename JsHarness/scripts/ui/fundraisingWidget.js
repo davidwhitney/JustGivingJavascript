6c874c513b86ebab9cd967b4f558f3e4949a2973
David Whitney <david@davidwhitney.co.uk>
2011-09-25 02:18:46 +0100

    crap ui

--- JsHarness/scripts/ui/fundraisingWidget.js
+++ JsHarness/scripts/ui/fundraisingWidget.js
@@ -18,9 +18,19 @@
         var title = $("<p class='title'>" + page.title + "</p>");
         var charity = $("<p class='charityName'>" + page.charity.name + "</p>");
 
+        var styleDetails = "<style type=\"text/css\">";
+        styleDetails += ".justGivingStatus { background-color: red; width: 240px; height: 400px; }";
+        styleDetails += ".title { font-weight: bolder; }";
+        styleDetails += ".charityName {  }";
+        styleDetails += ".thermometer {  }";
+        styleDetails += "</style>";
+
+        var style = $(styleDetails);
+
         wrapper.append(thermometer);
         wrapper.append(title);
         wrapper.append(charity);
+        wrapper.append(style);
 
         return {
             element: wrapper
