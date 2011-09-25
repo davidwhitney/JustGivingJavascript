74f79b7bf16d95e0f9dca5ebcbcee9a537987a8d
David Whitney <david@davidwhitney.co.uk>
2011-09-25 12:14:40 +0100

    added combiner

---
+++ Combiner/Combiner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Combiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var combineFile = args[0] ?? "jg.combine";
+            var fileList = File.ReadAllLines(combineFile);
+
+            var sb = new StringBuilder();
+            foreach(var filename in fileList)
+            {
+                sb.Append(File.ReadAllText(filename));
+            }
+
+            File.WriteAllText(combineFile.Replace(".combine", ".js"), sb.ToString());
+        }
+    }
+}
