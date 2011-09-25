d152ea6725ce93ffd15a8af3f2998d13cb330891
David Whitney <david@davidwhitney.co.uk>
2011-09-25 03:09:18 +0100

    fancyish ui

--- JsHarness/scripts/ui/fundraisingWidget.js
+++ JsHarness/scripts/ui/fundraisingWidget.js
@@ -18,7 +18,9 @@
         var image = $("<div class='image'><img src='http://www.justgiving.com/Utils/imaging.ashx?imageType=frpphoto&img=" + page.image.url + "&width=230' /></div>");
         var title = $("<p class='title'>" + page.title + "</p>");
         var charity = $("<p class='charityNameParagraph'><span class='charityName'>" + page.charity.name + "</span></p>");
-        var donate = $("<p class='donate'><span class='donateButton'>Donate</span></p>");
+        var raised = $("<p class='raised'>Raised: " + page.currencySymbol + page.totalRaisedOnline + "</p>");
+        var target = $("<p class='target'>Target: " + page.currencySymbol + page.fundraisingTarget + "</p>");
+        var donate = $("<p class='donate'><span class='donateButton'><a href=\"http://www.justgiving.com/"+ page.pageShortName +"/donate\">Donate</span></p>");
 
         var styleDetails = "<style type=\"text/css\">";
         styleDetails += ".justGivingStatus { background-color: #E6E4DE; width: 240px; height: 400px; font-family: Arial,Helvetica,sans-serif; }";
@@ -28,17 +30,22 @@
         styleDetails += ".charityName { font-weight: bolder; }";
         styleDetails += ".image { padding: 5px;  }";
         styleDetails += ".jgLogo { padding: 5px;  }";
+        styleDetails += ".raised { padding: 5px; }";
+        styleDetails += ".target { padding: 5px; }";
         styleDetails += ".donate { padding: 5px; }";
         styleDetails += ".donateButton { background: #617FBB; display: block; padding: 5px; font-weight: bolder; color: white; text-align: center; }";
+        styleDetails += ".donateButton a { color: white; text-decoration: none;}";
         styleDetails += "</style>";
 
         var style = $(styleDetails);
 
+        wrapper.append(logo);
         wrapper.append(title);
         wrapper.append(charity);
         wrapper.append(image);
+        wrapper.append(raised);
+        wrapper.append(target);
         wrapper.append(donate);
-        wrapper.append(logo);
         wrapper.append(style);
 
         return {
