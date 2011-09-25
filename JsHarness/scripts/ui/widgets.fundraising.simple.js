ceebd6d7c2838d3acbdf5b58918aabd736e7149b
Sam Salisbury <samsalisbury@gmail.com>
2011-09-25 12:02:22 +0100

    Added theme support for simple widget, WIP

--- JsHarness/scripts/ui/widgets.fundraising.simple.js
+++ JsHarness/scripts/ui/widgets.fundraising.simple.js
@@ -1,39 +1,42 @@
 ﻿(function ($) {
 
-    $.extend(jg.ui.widgets, {
+    var simple = function (target, pageShortName) {
+        target = jg.ui.utils.getJQueryObject(target);
+        jg.data.page.retrieve(pageShortName, function (page) {
+            target.append(createWidget(page).element);
+        })
+    };
 
-        simple: function (target, pageShortName) {
-            target = jg.ui.utils.getJQueryObject(target);
-            jg.data.page.retrieve(pageShortName, function (page) {
-                target.append(createWidget(page).element);
-            });
+    simple.themes = {
+        dark: {
+            background: "black",
+            foreground: "#72635c",
+            highlight: "pink"
         }
+    };
 
-    });
+    $.extend(jg.ui.widgets, { simple: simple });
 
     var createWidget = function (page) {
-        var wrapper = $("<div />");
 
         var styleMarkup = "<style type='text/css'>"
-            + "    .jg-text-widget { width:240px; background:black; color:#72635c;"
+            + "    .jg-text-widget { width:240px; background:{background}; color:{foreground};"
             + "                        font-family: Helvetica, Cambria, Arial, Sans-Serif;"
             + "                        font-weight: bold; font-size:16px; line-height: 28px;"
             + "                        float: left; margin:30px;}"
             + ""
             + "    .jg-text-widget p { display:block; margin:0; padding: 15px; }"
             + ""
-            + "    .jg-text-widget .charity-name { color:Pink; }"
-            + "    .jg-text-widget .raised-so-far { color:Pink; }"
-            + "    .jg-text-widget .left-to-raise { color:Pink; }"
-            + "    .jg-text-widget .donate-link { color:Pink; }"
+            + "    .jg-text-widget .charity-name { color:{highlight}; }"
+            + "    .jg-text-widget .raised-so-far { color:{highlight}; }"
+            + "    .jg-text-widget .left-to-raise { color:{highlight}; }"
+            + "    .jg-text-widget .donate-link { color:{highlight}; }"
             + ""
             + "    .jg-text-widget .logo { width:102.5px; height:23.5px;"
             + "                            padding:0 15px 15px 15px; }"
             + ""
             + "    .jg-text-widget .call-to-action { margin-top:-15px; }"
             + "</style>";
-
-        var style = $(styleMarkup);
 
         var widgetMarkup = "<section class='jg-text-widget'>"
             + "    <p>"
@@ -59,20 +62,31 @@
             pageUrl: jg.settings.websiteUrl + "/" + page.pageShortName
         };
 
+        var theme = jg.ui.widgets.simple.themes.dark;
+
+        widgetMarkup = renderTemplate(widgetMarkup, model);
+        styleMarkup = renderTemplate(styleMarkup, theme);
+
+        var widget = $(widgetMarkup);
+        var style = $(styleMarkup);
+        widget.append(style);
+
+        return {
+            element: widget
+        };
+    };
+
+    var templateTokenRegex = /\{([a-z0-9\.]+)\}/gmi;
+
+    var renderTemplate = function (template, model) {
         var templateTokenRegex = /\{([a-z0-9\.]+)\}/gmi;
         var match = null;
 
-        while (match = templateTokenRegex.exec(widgetMarkup)) {
-            widgetMarkup = widgetMarkup.replace(match[0], model[match[1]]);
+        while (match = templateTokenRegex.exec(template)) {
+            template = template.replace(match[0], model[match[1]]);
         }
 
-        var widget = $(widgetMarkup);
-        wrapper.append(style);
-        wrapper.append(widget);
-
-        return {
-            element: wrapper
-        };
+        return template;
     };
 
 } (jQuery));
